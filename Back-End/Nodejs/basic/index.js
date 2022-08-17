4dc5c8fb0bf21112a2430cc64b6a7b825a3c06ae
GASADER <kajorhkaen@gmail.com>
2022-08-18 03:42:44 +0700

    Routing

--- Back-End/Nodejs/basic/index.js
+++ Back-End/Nodejs/basic/index.js
@@ -1,8 +1,16 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    res.write("<h2>Hello node.js</h2>")
-    res.end()
+    const pathName = req.url
+    console.log(pathName)
+    if(pathName==="/" || pathName==="/home"){
+        res.end("<h1>Hello Homepage</h1>")
+    }else if(pathName==="/product"){
+        res.end("<h1>Hello Product</h1>")
+    }else{ 
+        res.writeHead(404)
+        res.end("<h1>Not Found</h1>")
+    }
 })
 .listen(3000,()=>{
     console.log("start server")

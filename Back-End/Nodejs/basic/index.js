20ed21cde75ba3e74265a644281a9079e6ba55ed
GASADER <kajorhkaen@gmail.com>
2022-08-24 18:17:45 +0700

    modify index

--- Back-End/Nodejs/basic/index.js
+++ Back-End/Nodejs/basic/index.js
@@ -4,7 +4,7 @@ const server = http.createServer((req,res)=>{
     const pathName = req.url
     console.log(pathName)
     if(pathName==="/" || pathName==="/home"){
-        res.end("<h1>Hello Homepage</h1>")
+        res.end("<h1>Hello Home</h1>")
     }else if(pathName==="/product"){
         res.end("<h1>Hello Product</h1>")
     }else{ 

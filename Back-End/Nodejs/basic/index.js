b30c31c16fde823636aabc7298fcfaf901ff7dc0
GASADER <kajorhkaen@gmail.com>
2022-08-25 03:39:50 +0700

    edit Html

--- Back-End/Nodejs/basic/index.js
+++ Back-End/Nodejs/basic/index.js
@@ -2,6 +2,7 @@ const http = require('http')
 const fs = require('fs')
 
 const indexpage = fs.readFileSync(`${__dirname}/webpage/k-shop templates/index.html`)
+const productPage = fs.readFileSync(`${__dirname}/webpage/k-shop templates/product.html`,'utf-8')
 
 const server = http.createServer((req,res)=>{
 
@@ -10,7 +11,7 @@ const server = http.createServer((req,res)=>{
     if(pathName==="/" || pathName==="/home"){
         res.end(indexpage)
     }else if(pathName==="/product"){
-        res.end("<h1>Hello Product</h1>")
+        res.end(productPage)
     }else{ 
         res.writeHead(404)
         res.end("<h1>Not Found</h1>")
